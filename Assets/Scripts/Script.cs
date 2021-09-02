0f1e231dc6262dd462e566fc722bfead040ae16a
iisafca <iisafca@outlook.com>
2021-09-03 00:09:17 +0300

    integer olusturuldu

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -7,7 +7,7 @@ public class Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 0;
     }
 
     // Update is called once per frame
