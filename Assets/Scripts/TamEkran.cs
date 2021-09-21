1772f26777d7f7b244cd94a680f22b9186e0e9fb
iisafca <iisafca@outlook.com>
2021-09-21 16:18:11 +0300

    Arkaplan görüntüsü ekran boyutlarına uygun hale getirildi

--- Assets/Scripts/TamEkran.cs
+++ Assets/Scripts/TamEkran.cs
@@ -7,7 +7,16 @@ public class TamEkran : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Arka plan goruntusunu ekran byutuna uygun hale getirdik.
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 tempScale = transform.localScale;
+
+        float spriteGenislik = spriteRenderer.size.x;
+        float ekranYukseklik = Camera.main.orthographicSize * 2.0f;
+        float ekranGenislik = ekranYukseklik / Screen.height * Screen.width;
+        tempScale.x = ekranGenislik / spriteGenislik;
+        transform.localScale = tempScale;
+
     }
 
     // Update is called once per frame
